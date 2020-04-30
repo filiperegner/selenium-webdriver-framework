72bacb09528fc3f4943423c3e8402e58f4096140
filiperegner <35273309+filiperegner@users.noreply.github.com>
2020-04-30 08:58:06 -0300

    framework finished.

--- src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
+++ src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
@@ -1,10 +1,17 @@
 package com.herokuapp.theinternet.base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class BrowserDriverFactory {
 
@@ -32,9 +39,34 @@ public class BrowserDriverFactory {
 			driver.set(new FirefoxDriver());
 			break;
 
-		default:
-			log.info("Do not know how to start: " + browser + ", starting chrome.");
+		case "chromeheadless":
 			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--headless");
+			driver.set(new ChromeDriver(chromeOptions));
+			break;
+
+		case "firefoxheadless":
+			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+			FirefoxBinary firefoxBinary = new FirefoxBinary();
+			firefoxBinary.addCommandLineOptions("--headless");
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setBinary(firefoxBinary);
+			driver.set(new FirefoxDriver(firefoxOptions));
+			break;
+
+		case "phantomjs":
+			System.setProperty("phantomjs.binary.path", "src/main/resources/phantomjs");
+			driver.set(new PhantomJSDriver());
+			break;
+
+		case "htmlunit":
+			driver.set(new HtmlUnitDriver());
+			break;
+
+		default:
+			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 			driver.set(new ChromeDriver());
 			break;
 		}
@@ -46,6 +78,18 @@ public class BrowserDriverFactory {
 		log.info("Starting chrome driver with profile: " + profile);
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("user-data-dir=src/main/resources/Profiles/" + profile);
+
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		driver.set(new ChromeDriver(chromeOptions));
+		return driver.get();
+	}
+
+	public WebDriver createChromeWithMobileEmulation(String deviceName) {
+		log.info("Starting driver with " + deviceName + " emulation]");
+		Map<String, String> mobileEmulation = new HashMap<>();
+		mobileEmulation.put("deviceName", deviceName);
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
 
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		driver.set(new ChromeDriver(chromeOptions));
